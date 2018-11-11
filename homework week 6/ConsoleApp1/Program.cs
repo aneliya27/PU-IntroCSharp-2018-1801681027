597610db51056ba1ccba4b97722c4a94262aef48
Ani Belcheva <ani.belcheva@abv.bg>
2018-11-11 15:23:40 +0200

    x

---
+++ homework week 6/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose option (triangle, square, radians)");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                 case "triangle":
+                 Console.WriteLine($"enter side:");
+                 double side = double.Parse(Console.ReadLine());
+                 Console.WriteLine("enter height:");
+                 double height = double.Parse(Console.ReadLine());
+                 double trianglearea= side*height/2;
+
+                 Console.WriteLine($"the area of the triangle is {trianglearea}"  );
+
+                break;
+                case "square":
+                 Console.WriteLine($"enter side:");
+                 double squareside = double.Parse(Console.ReadLine());
+                 double squarearea = Math.Pow(squareside,2);
+
+
+                 Console.WriteLine($"the area of the square is {squarearea}");
+                 break;
+                 case "radians":
+                    Console.WriteLine("Enter the degrees you want to transform in radians");
+                    double degrees = double.Parse(Console.ReadLine());
+                    double radians = (Math.PI * degrees) / 180;
+                    Console.WriteLine($"Your degrees in radians are = {radians}");
+                   break;
+                default:
+                    Console.WriteLine("Incorrect option!");
+                    break;
+             
+
+
+
+            }
+        }
+    }   }
---
+++ homework week 6/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Smile
+
+{
+    class Program
+    {
+        private static IEnumerable<string> students;
+
+        static void Main(string[] args)
+
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string[] student = System.IO.File.ReadAllLines(@"C:\Users\Ani\Desktop\Уни\C#\PU-IntroCSharp-2018-1801681027\homework week 6\ConsoleApp2\file\students.txt");
+            int studentSum = 0;
+            int smiley = '\u263a';
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in student)
+            {
+                student = line.Split(' ');
+                string firstName = student[1];
+                string fathersName = student[2];
+                string lastName = student[3];
+                string fullName = $"{firstName} {fathersName} {lastName}";
+
+                for (int i = 0; i < fullName.Length; i++)
+                {
+                    studentSum = studentSum + (int)fullName[i];
+                }
+                if (studentSum > 15000)
+                    Console.WriteLine($" {fullName} {(char)smiley} => {studentSum}");
+
+
+
+            }
+        }
+    }
+}
